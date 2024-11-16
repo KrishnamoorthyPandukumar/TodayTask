299c958448e3333b71a8412a113dda7907224527
KrishnamoorthyPandukumar <92204796+KrishnamoorthyPandukumar@users.noreply.github.com>
2024-11-16 21:27:00 +0530

    Initial Commit

--- todaytask/src/com/samples/ReverseString.java
+++ todaytask/src/com/samples/ReverseString.java
@@ -6,17 +6,29 @@ public class ReverseString {
 
     public static void main(String[] args) {
 
+        System.out.println(ReverseString.stringReverseStreamApi("He"));
+        System.out.println(ReverseString.reverseString("eH"));
     }
 
 
-    public String stringReverseStreamApi(String rev){
+    public static String reverseString(String str){
+        String reversed = "";
 
-        String reversed = rev.chars()                    // Create an IntStream of character codes (ASCII values)
-                .mapToObj(c -> (char) c)  // Convert the int stream to a Stream<Character>
-                .collect(Collectors.toCollection(StringBuilder::new)) // Collect into StringBuilder
-                .reverse()                // Reverse the StringBuilder
-                .toString();
+        // Loop through the original string in reverse order
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
 
-        retrun reversed;
+    public static String stringReverseStreamApi(String rev){
+
+        String reversed = rev.chars()                               // IntStream of character codes (ASCII values)
+                .mapToObj(c -> (char) c)                              // Convert the int stream to a Stream<Character>
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append) // Collect into StringBuilder
+                .reverse()                                           // Reverse the StringBuilder
+                .toString();                                         // Convert StringBuilder to string
+
+        return reversed;
     }
 }
