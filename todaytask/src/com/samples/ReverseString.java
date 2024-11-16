30ef8fcf5d12cb411dc53cb2a1b248a4c4149043
KrishnamoorthyPandukumar <92204796+KrishnamoorthyPandukumar@users.noreply.github.com>
2024-11-16 21:18:41 +0530

    Initial Commit

--- todaytask/src/com/samples/ReverseString.java
+++ todaytask/src/com/samples/ReverseString.java
@@ -1,9 +1,22 @@
 package com.samples;
 
+import java.util.stream.Collectors;
+
 public class ReverseString {
 
     public static void main(String[] args) {
 
     }
 
+
+    public String stringReverseStreamApi(String rev){
+
+        String reversed = rev.chars()                    // Create an IntStream of character codes (ASCII values)
+                .mapToObj(c -> (char) c)  // Convert the int stream to a Stream<Character>
+                .collect(Collectors.toCollection(StringBuilder::new)) // Collect into StringBuilder
+                .reverse()                // Reverse the StringBuilder
+                .toString();
+
+        retrun reversed;
+    }
 }
